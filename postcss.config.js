42713dfc5730195576e9132a4baed53d1c14b374
James Sheen <jimsheen@hotmail.com>
2018-10-30 09:39:07 +0000

    postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -4,5 +4,22 @@ module.exports = {
     // The order is important here
     require('postcss-import'),
     require('postcss-nested'),
+    require('postcss-extend'),
+    require('postcss-mixins'),
+    require('postcss-at-rules-variables'),
+    require('postcss-each'),
+    require('postcss-for'),
+    require('postcss-conditionals'),
+    require('lost'),
+    require('postcss-cssnext'),
+    require('postcss-math'),
+    require('postcss-hexrgba'),
+
+    // These are always nice to have
+    require('postcss-plugin-px2rem')({
+      rootValue: 16,
+      unitPrecision: 4,
+      minPixelValue: 1
+    })
   ]
 };
