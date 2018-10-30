a621d3ffaccc781bd4d090e565aeb912eb95af83
James Sheen <jimsheen@hotmail.com>
2018-10-30 10:00:58 +0000

    added default vars and reset

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -49,9 +49,22 @@ const getStyleLoaders = (cssOptions, preProcessor) => {
         // https://github.com/facebook/create-react-app/issues/2677
         ident: 'postcss',
         plugins: () => [
-          require('postcss-flexbugs-fixes'),
           require('postcss-import'),
           require('postcss-nested'),
+          require('postcss-extend'),
+          require('postcss-mixins'),
+          require('postcss-at-rules-variables'),
+          require('postcss-each'),
+          require('postcss-for'),
+          require('postcss-flexbugs-fixes'),
+          require('postcss-conditionals'),
+          require('postcss-math'),
+          require('postcss-hexrgba'),
+          require('postcss-plugin-px2rem')({
+            rootValue: 16,
+            unitPrecision: 4,
+            minPixelValue: 1
+          }),
           require('postcss-preset-env')({
             autoprefixer: {
               flexbox: 'no-2009',
